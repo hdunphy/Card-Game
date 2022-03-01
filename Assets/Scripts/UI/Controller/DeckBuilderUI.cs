5ef9484cb26990120082a21d0538f83766ad770d
Henry Dunphy <hdunphy15@gmail.com>
2022-03-01 07:54:15 -0500

    Added Mock UI for Deck Builder

--- Assets/Scripts/UI/Controller/DeckBuilderUI.cs
+++ Assets/Scripts/UI/Controller/DeckBuilderUI.cs
@@ -6,7 +6,7 @@ public class DeckBuilderUI : MonoBehaviour
 
     private void Start()
     {
-        DeckBuilderCanvas.gameObject.SetActive(false);
+        //DeckBuilderCanvas.gameObject.SetActive(false);
     }
 
     public void Show()
