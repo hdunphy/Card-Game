9f33cff5bc4aaee63683115c3d883c4abbd900cf
Henry Dunphy <hdunphy15@gmail.com>
2022-12-30 12:41:07 -0500

    Added Mingming Storage Container Controller

---
+++ Assets/Scripts/UI/GameScene/MingmingStorage/MingmingStorageContainerController.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts.Entities.Player;
+using Assets.Scripts.Entities.Scriptable;
+using Assets.Scripts.Helpers;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.GameScene.MingmingStorage
+{
+    public class MingmingStorageContainerController : MonoBehaviour
+    {
+        [SerializeField] private Transform mingmingStorageTransform;
+        [Header("Prefabs")]
+        [SerializeField] private GameObject horizontalRowPrefab;
+        [SerializeField] private StoredMingmingController storedMingmingPrefab;
+
+        [SerializeField] public List<MingmingLevelData> Mingmings;
+
+        private void Start()
+        {
+            Setup(new(Mingmings));
+        }
+
+        public void Setup(PlayerMingmingHolder mingmingHolder)
+        {
+            var rows = mingmingHolder.AllMingmings.ChunkBy(3);
+            rows.ForEach(row => {
+                var horizontalRow = Instantiate(horizontalRowPrefab, mingmingStorageTransform);
+                row.ForEach(mingming =>
+                {
+                    var storedMingming = Instantiate(storedMingmingPrefab, horizontalRow.transform);
+                    storedMingming.Setup(mingming);
+                });
+            });
+        }
+    }
+}
